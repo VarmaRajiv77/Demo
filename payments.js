85f01a114140cc9d858314e3abf51abd3d0bda84
EC2 Default User <ec2-user@ip-172-31-0-246.us-west-1.compute.internal>
2022-07-09 05:46:55 +0000

    Version updated

--- payments.js
+++ payments.js
@@ -4,9 +4,9 @@ node {
 
 	stage 'Build'
 		bat 'nuget restore SolutionName.sln'
-		bat "\"${tool 'MSBuild'}\" SolutionName.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
+		bat "\"${tool<Down> 'MSBuild'}\" SolutionName.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.1.0.${env.BUILD_NUMBER}"
 
-	stage 'Archive'
+	stage 'Deploy'
 		archive 'ProjectName/bin/Release/**'
 
 }
